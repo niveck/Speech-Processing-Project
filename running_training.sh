71691802b62d0de6e6a8f793f3a144f4b969c429
niveck <niv.eckhaus@gmail.com>
2024-09-03 11:44:07 +0300

    modified training bash script

--- running_training.sh
+++ running_training.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #SBATCH --mem=32gb
 #SBATCH -c2
-#SBATCH --time=3-0:0:0
+#SBATCH --time=3-0
 #SBATCH --gres=gpu:1,vmem:32g
 #SBATCH --error=error_log_job%A.txt
 #SBATCH --output=output_log_job%A.txt
 #SBATCH --job-name=ctc_llm_training_290824
+#SBATCH --mail-user=niv.eckhaus@mail.huji.ac.il
+#SBATCH --mail-type=ALL
 
 cd /cs/snapless/gabis/nive/speech/Speech-Processing-Project/
 module load cuda/11.7
-../../venvs/async_env/bin/python final_project.py
+module load nvidia
+source ../../venvs/async_env/bin/activate
+python final_project.py
